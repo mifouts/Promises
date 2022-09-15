cbe6c5e4dabf7876cd7f1f26d8cfcb8e7822aa95
mifouts <fouts.mitch15@gmail.com>
2022-09-15 15:33:27 -0400

    practice

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ fetch("https://jsonplaceholder.typicode.com/users/1")
 // 1. Then
 
 fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
-    console.log(response.json())
-    response.json().then((data) => {})
+    response.json().then(data => {console.log(data)})
 });
 
 // 2. Async Await
