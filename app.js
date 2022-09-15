a85fe11d859de0ed6b72c43d38aaadae154fdfd9
mifouts <fouts.mitch15@gmail.com>
2022-09-15 15:50:25 -0400

    cleaning up code

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // fetch("https://jsonplaceholder.typicode.com/users/1")
 const emailRef = document.querySelector(".email");
+
+
 // 1. Then
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
-    response.json().then(data => {
-        emailRef.innerHTML = data.email;
-    })
+fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+    return response.json()
+}).then(data => {
+    console.log(data);
 });
 
 // 2. Async Await
