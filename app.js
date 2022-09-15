53f2d184b3b865ccfea6eb8445ca5bf9a6df5134
mifouts <fouts.mitch15@gmail.com>
2022-09-15 15:41:36 -0400

    working through practice problem

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-fetch("https://jsonplaceholder.typicode.com/users/1")
-
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+const emailRef = document.querySelector(".email");
+console.log(emailRef)
 // 1. Then
 
 fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
-    response.json().then(data => {console.log(data)})
+    response.json().then(data => {
+        console.log(data)
+        emailRef.innerHTML = data.name
+    })
 });
 
 // 2. Async Await
