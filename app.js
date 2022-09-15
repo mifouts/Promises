f7e9e0f6731646f68e1feedd0d347d6d151e5db2
mifouts <fouts.mitch15@gmail.com>
2022-09-15 15:57:01 -0400

    practicing

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const emailRef = document.querySelector(".email");
 
 // 1. Then
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-    return response.json()
-}).then(data => {
-    console.log(data);
-});
+//fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+  //  return response.json()
+//}).then(data => {
+   // console.log(data);
+//});
 
 // 2. Async Await
 
+async function main(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await response.json()
+    console.log(data)
+    emailRef.innerHTML = data.email
+}
+
+main();
