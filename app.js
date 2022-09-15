ca471c15a8718fb08fc12cdb53e2c4f77d361fdd
mifouts <fouts.mitch15@gmail.com>
2022-09-15 15:30:58 -0400

    promise practice

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ fetch("https://jsonplaceholder.typicode.com/users/1")
 // 1. Then
 
 fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
-    console.log(response);
+    console.log(response.json());
 });
 
